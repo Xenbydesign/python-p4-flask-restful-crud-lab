48c65b8dde323f31629c9e6ef6f0d4b98449bea8
Xennia Contreras <Xenbydesign@gmail.com>
2024-04-04 17:21:35 -0700

    done

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, g
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -27,9 +27,9 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
         )
 
         db.session.add(new_plant)
@@ -38,7 +38,7 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
@@ -47,9 +47,33 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        try:
+            if plant := db.session.get(Plant, id):
+                data = request.json
+                for attr, value in data.items():
+                    setattr(plant, attr, value)
+                db.session.commit()
+                return make_response(plant.to_dict(), 200)
+            return {"error": f"plant #{id} not found."}
+        except Exception as e:
+            db.session.rollback()
+            return str(e), 422
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+    def delete(self, id):
+        try:
+            if plant := db.session.get(Plant, id):
+                db.session.delete(plant)
+                db.session.commit()
+                return "", 204
+            return {"error": f"plant #{id} not found."}
+        except Exception as e:
+            db.session.rollback()
+            return str(e), 422
 
 
-if __name__ == '__main__':
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
